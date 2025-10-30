2495436382f67eb1e02a11309a7ebac71a620dd2
JessicaSandre <jessicasandrebeserra@gmail.com>
2025-10-29 21:40:45 -0300

    Botão Whatsapp

--- produtos/produtoscript.js
+++ produtos/produtoscript.js
@@ -1,7 +1,7 @@
 function openTab(event, tabName) {
     // Esconde todo o conteúdo das abas
     const tabContents = document.querySelectorAll('.tab-content');
-    tabContents.forEach(tab => tab.classList.remove('active'));
+    tabContents.forEach(tab => tab.classList.remove('active')); 
 
     // Remove a classe 'active' de todos os botões
     const tabButtons = document.querySelectorAll('.tab-button');
